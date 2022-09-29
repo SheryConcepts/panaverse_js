f9de7071283d53741be87906d2147091880e27c2
Shery <chsheharyarahmed@gmail.com>
2022-09-29 18:54:54 +0500

    ex 29 done

---
+++ favourite-fruit.js
@@ -0,0 +1,17 @@
+const fav_fruit = ["banana", "apple", "pineapple", "grapes", "avacado"];
+
+if (fav_fruit.includes("banana")) {
+    console.log("banana is my fav fruit");
+}
+if (fav_fruit.includes("apple")) {
+    console.log("apple is my fav fruit");
+}
+if (fav_fruit.includes("pineapple")) {
+    console.log("pineapple is my fav fruit");
+}
+if (fav_fruit.includes("grapes")) {
+    console.log("grapes is my fav fruit");
+}
+if (fav_fruit.includes("avacado")) {
+    console.log("avacado is my fav fruit");
+}
