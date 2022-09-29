2c9f50196679dd76b3ef5f9aa1e2d6efe19b80a3
Shery <chsheharyarahmed@gmail.com>
2022-09-29 18:34:04 +0500

    ex 24 done

---
+++ 18-conditionals.js
@@ -0,0 +1,10 @@
+let car = "Sabaru";
+if (car === "Sabaru") {
+    console.log("Sabaru")
+} else if (car === "Mehran") {
+    console.log("Mehran")
+} else if (car === "Alto") {
+    console.log("Alto")
+} else if (1 != 2) {
+    console.log(1)
+}
---
+++ 19-more-conditionals.js
@@ -0,0 +1,24 @@
+if (1 === 2) {
+    console.log(1);
+}
+if (1 > 2) {
+    console.log(2)
+}
+
+if ("123" === "123") {
+    console.log(true);
+}
+
+if (1 < 2) {
+    console.log(2)
+}
+
+const a = [1,2,3];
+const b = [1,2,3];
+if (a === b) {
+    console.log("a = b")
+}
+
+if (a.includes(3)){
+    console.log("3 is in a")
+}
