e73258473b91b2765952fc070dc9a6a34fd06f18
Shery <chsheharyarahmed@gmail.com>
2022-09-29 18:42:10 +0500

    ex 27 done

---
+++ alien-colors-v3.js
@@ -0,0 +1,8 @@
+const alien = "red";
+if (alien === "red") {
+    console.log("15 points are earned");
+} else if (alien === "yellow") {
+    console.log("10 points are earned");
+} else {
+    console.log("5 points are earned");
+}
