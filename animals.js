bae4d6b0c591d6e3a842ac7292db077521dfc90c
Shery <chsheharyarahmed@gmail.com>
2022-09-29 19:58:48 +0500

    ex 35 done

---
+++ animals.js
@@ -0,0 +1,12 @@
+const animals = ["dog", "cat", "mouse"];
+
+for (let i = 0; i < animals.length; ++i) {
+    if (animals[i] === "dog") {
+        console.log("a dog would be great pet")
+    } else if (animals[i] === "cat") {
+        console.log("cat is a fiesty pet")
+    } else if (animals[i]==="mouse") {
+        console.log("i dont know about this one tho")
+    }
+}
+console.log("all of these pets have four legs")
